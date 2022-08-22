886af71ddae21419df34015417697fb658961290
beckireby15@gmail.com <beckireby15@gmail.com>
2022-08-22 17:52:33 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+function findMatching(drivers, name) {
+  return drivers.filter(function (driver) {
+    return driver.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, middleName) {
+  return drivers.filter(function (driver) {
+    return (
+      driver.slice(0, middleName.length).toLowerCase() ===
+      middleName.toLowerCase()
+    );
+  });
+}
+
+function matchName(drivers, property) {
+  return drivers.filter(function (driver) {
+    return driver.name.toLowerCase() === property.toLowerCase();
+  });
+}
